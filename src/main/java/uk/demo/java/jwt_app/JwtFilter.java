31f46df39962b1c5ad4b6f621d2912d4dfa2f199
anicieja <alice.nicieja@gmail.com>
2020-12-28 14:39:39 +0100

    init commit

--- src/main/java/uk/demo/java/jwt_app/JwtFilter.java
+++ src/main/java/uk/demo/java/jwt_app/JwtFilter.java
@@ -14,6 +14,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.Collections;
 
 
@@ -30,17 +40,53 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(String authorization) {
-        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC512("7w!z%C*F-J@NcRfUjXn2r5u8x/A?D(G+KbPdSgVkYp3s6v9y$B&E)H@McQfThWmZ"))
-                .build();
+
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.RSA256((RSAPublicKey) getPublicKey(), (RSAPrivateKey) getPrivateKey())).build();
         DecodedJWT verify = jwtVerifier.verify(authorization.substring(7));
         String name = verify.getClaim("name").asString();
         Boolean isAdmin = verify.getClaim("admin").asBoolean();
+        String role = getRole(isAdmin);
+
+       return new UsernamePasswordAuthenticationToken(name, null, Collections.singleton(new SimpleGrantedAuthority(role)));
+    }
+
+    private String getRole(Boolean isAdmin) {
         String role = "ROLE_USER";
-
         if (isAdmin) role = "ROLE_ADMIN";
+        return role;
+    }
 
-        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(role);
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(name, null, Collections.singleton(simpleGrantedAuthority));
-        return token;
+    private PublicKey getPublicKey()  {
+
+        String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMNQULyyizs5NCCPnifcAS41diI3oZzU" +
+                "r0tpJU6TBdw8tRAt3mN2ZsYkPK60mjjMDrFwOYXX9vdJHnHSI+buk0UCAwEAAQ==";
+
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
+        try {
+            return KeyFactory.getInstance("RSA").generatePublic(keySpec);
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private PrivateKey getPrivateKey() {
+
+        String privateKey = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAw1BQvLKLOzk0II+e" +
+                "J9wBLjV2IjehnNSvS2klTpMF3Dy1EC3eY3ZmxiQ8rrSaOMwOsXA5hdf290kecdIj" +
+                "5u6TRQIDAQABAkEAkbeXNOFETWAlSvG7fmN+ofoS8/5rXfWz/uAojFHWenOlzUau" +
+                "mAhdFjwNcIdc8qkfojrQzHd8wHTkWLqGK0bpwQIhAPENVbdlJd+npin70txczY+P" +
+                "/TRUqhqj5VOGUjSKxXPRAiEAz2zkHzA/bBAlWbUntuKgnRJjmIH9pir41Vig2OJX" +
+                "STUCIC8HPPvkvfjeimqSeNcJPAmQPAQjqHQ+GZWsFQmvMUqhAiEAwlqWeT68/mU2" +
+                "ihK6zpsUwXg8h+atI2i6VTVBKVcTUE0CICDTNIzA8pGo6fkLN4K7zrF5KwptKPYA" +
+                "NIQXPVtvmCuU";
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey));
+        try {
+            return KeyFactory.getInstance("RSA").generatePrivate(keySpec);
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
